d6f02bca6580c0b42a8c4d574a49ef8f1678317f
WehrWolff <135031808+WehrWolff@users.noreply.github.com>
2023-09-09 22:38:55 +0200

    Implemented token

--- src/lexer.h
+++ src/lexer.h
@@ -1,19 +1,22 @@
+#include <iostream>
 #include <string>
 #include <list>
 #include <regex>
 #include <stdexcept>
 
-class Lexer{
+class Lexer {
     private:
         //std::regex_match("subject", std::regex("\s"))
-        const string linechange = "\n";
-        string ignore = " \t";
-        string line_comment = "~";
-        string block_comment = "===";
 
-        std::list<string> _literals; //{"+", "-", "*", "/"};
+        const std::string linechange = "\n";
 
-        enum Token{
+        std::string ignore = " \t";
+        std::string line_comment = "~";
+        std::string block_comment = "===";
+
+        std::list<string> literals; //{"+", "-", "*", "/"};
+
+        enum Token {
             TOKEN_NUMBER = -1, //"NUMBER"
             TOKEN_STRING = -2, //"STRING"
             TOKEN_VAR = -3, //"VAR"
@@ -24,12 +27,50 @@ class Lexer{
         };
 
     public:
-        void setLiterals(std::list<string> literals){
-            for (string literal : literals) {
+        void setIgnore (_ignore) {
+            ignore = _ignore;
+        }
+
+        void setLineComment (_line_comment) {
+            line_comment = _line_comment;
+        }
+
+        void setBlockComment (_block_comment) {
+            block_comment = _block_comment;
+        }
+
+        void setLiterals (std::list<string> _literals) {
+            for (std::string literal : _literals) {
                 if (literal.size() != 1) {
                     throw std::invalid_argument("literals must be of size 1");
                 }
             }
-            _literals = literals;
+            literals = _literals;
         }
 };
+
+class Token {
+    private:
+        std::string tok;
+        auto value;
+
+        std::string regex;
+    
+    public:
+        std::string getTok () {
+            return tok;
+        }
+
+        auto getValue () {
+            return value;
+        }
+
+        std::ostream& operator<< (std::ostream &s, const Token &token) {
+            return s << token.getTok() << ":" << token.getValue();
+        }
+
+};
+
+/* class Position {
+
+}; */
