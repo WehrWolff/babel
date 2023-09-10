8c008358234a8bc30f60e3ff102228655279782d
WehrWolff <135031808+WehrWolff@users.noreply.github.com>
2023-09-10 21:43:58 +0200

    Removed unnecessary enum

--- src/lexer.h
+++ src/lexer.h
@@ -16,17 +16,7 @@ class Lexer {
         std::string block_comment = "===";
 
         std::list<std::string> literals; //{"+", "-", "*", "/"};
-        std::map<std::string, std::string> keywords; // name -> regex
-
-        enum Token {
-            TOKEN_NUMBER = -1, //"NUMBER"
-            TOKEN_STRING = -2, //"STRING"
-            TOKEN_VAR = -3, //"VAR"
-        
-            TOKEN_IF = -4, //"IF"
-            TOKEN_ELSE = -5 //"ELSE"
-
-        };
+        std::map<std::string, std::string> keywords; // name -> regex        
 
     public:
         void setIgnore (std::string _ignore) {
