0a889553a26148eb2f002fdc85305f1d84702a3b
WehrWolff <135031808+WehrWolff@users.noreply.github.com>
2023-12-30 12:21:20 +0100

    Setup Conan

---
+++ babelbuild.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Make sure packages are installed
+conan install . --output-folder=./build --build=missing --settings=compiler.cppstd=20 --settings=build_type=Release
+
+# Create build dir if it does not exist
+mkdir build
+
+# Copy required files
+cp src/grammar.txt build/grammar.txt
+
+# Build commands
+cd build
+
+cmake .. -DCMAKE_TOOLCHAIN_FILE=./conan/build/Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+make
+
+# Run in shell
+./babel
